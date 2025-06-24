b41d48b8a8188b89fea6804cb08b9a530fc62a03
Airdox <beuth62@googlemail.com>
2025-06-24 10:51:50 +0200

    Add files via upload

---
+++ main_apk.py
@@ -0,0 +1,188 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.filechooser import FileChooserListView
+from kivy.uix.progressbar import ProgressBar
+from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelItem
+from kivy.clock import Clock
+import os
+import random
+
+class VoiceCloningApp(App):
+    def build(self):
+        # Hauptlayout erstellen
+        self.main_layout = BoxLayout(orientation='vertical')
+        
+        # TabbedPanel für die verschiedenen Funktionen
+        self.tabs = TabbedPanel(do_default_tab=False)
+        
+        # Tab 1: Stimmenmodell trainieren
+        self.train_tab = TabbedPanelItem(text='Stimmenmodell trainieren')
+        self.train_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        # Bereich für Audiodateien auswählen
+        self.train_layout.add_widget(Label(text='Audiodateien für Training auswählen:'))
+        
+        # Dateiauswahl
+        self.file_chooser = FileChooserListView(path=os.path.expanduser('~'))
+        self.train_layout.add_widget(self.file_chooser)
+        
+        # Button zum Auswählen der Dateien
+        self.select_button = Button(text='Dateien auswählen', size_hint=(1, 0.1))
+        self.select_button.bind(on_press=self.select_files)
+        self.train_layout.add_widget(self.select_button)
+        
+        # Ausgewählte Dateien anzeigen
+        self.selected_files_label = Label(text='Keine Dateien ausgewählt', size_hint=(1, 0.1))
+        self.train_layout.add_widget(self.selected_files_label)
+        
+        # Modellname eingeben
+        self.model_name_input = TextInput(hint_text='Modellname eingeben', size_hint=(1, 0.1))
+        self.train_layout.add_widget(self.model_name_input)
+        
+        # Training starten Button
+        self.train_button = Button(text='Training starten', size_hint=(1, 0.1))
+        self.train_button.bind(on_press=self.start_training)
+        self.train_layout.add_widget(self.train_button)
+        
+        # Fortschrittsbalken
+        self.progress_layout = BoxLayout(orientation='vertical', size_hint=(1, 0.2))
+        self.progress_label = Label(text='Fortschritt: 0%')
+        self.progress_bar = ProgressBar(max=100, value=0)
+        self.progress_layout.add_widget(self.progress_label)
+        self.progress_layout.add_widget(self.progress_bar)
+        self.train_layout.add_widget(self.progress_layout)
+        
+        # Log-Bereich
+        self.log_label = Label(text='Log-Ausgabe:', size_hint=(1, 0.1), halign='left')
+        self.log_label.bind(size=self.log_label.setter('text_size'))
+        self.train_layout.add_widget(self.log_label)
+        
+        self.log_output = Label(text='', size_hint=(1, 0.3), halign='left', valign='top')
+        self.log_output.bind(size=self.log_output.setter('text_size'))
+        self.train_layout.add_widget(self.log_output)
+        
+        self.train_tab.add_widget(self.train_layout)
+        self.tabs.add_widget(self.train_tab)
+        
+        # Tab 2: Text-to-Speech
+        self.tts_tab = TabbedPanelItem(text='Text-to-Speech')
+        self.tts_layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+        
+        # Modell auswählen
+        self.tts_layout.add_widget(Label(text='Stimmenmodell auswählen:'))
+        self.model_selection = Button(text='Modell auswählen', size_hint=(1, 0.1))
+        self.model_selection.bind(on_press=self.select_model)
+        self.tts_layout.add_widget(self.model_selection)
+        
+        # Ausgewähltes Modell anzeigen
+        self.selected_model_label = Label(text='Kein Modell ausgewählt', size_hint=(1, 0.1))
+        self.tts_layout.add_widget(self.selected_model_label)
+        
+        # Text eingeben
+        self.tts_layout.add_widget(Label(text='Text eingeben:'))
+        self.text_input = TextInput(hint_text='Text für Sprachsynthese eingeben', size_hint=(1, 0.3))
+        self.tts_layout.add_widget(self.text_input)
+        
+        # Sprachsynthese starten
+        self.synthesize_button = Button(text='Sprachsynthese starten', size_hint=(1, 0.1))
+        self.synthesize_button.bind(on_press=self.start_synthesis)
+        self.tts_layout.add_widget(self.synthesize_button)
+        
+        # Audio abspielen
+        self.play_button = Button(text='Audio abspielen', size_hint=(1, 0.1), disabled=True)
+        self.play_button.bind(on_press=self.play_audio)
+        self.tts_layout.add_widget(self.play_button)
+        
+        # Status anzeigen
+        self.tts_status = Label(text='Bereit', size_hint=(1, 0.1))
+        self.tts_layout.add_widget(self.tts_status)
+        
+        self.tts_tab.add_widget(self.tts_layout)
+        self.tabs.add_widget(self.tts_tab)
+        
+        # TabbedPanel zum Hauptlayout hinzufügen
+        self.main_layout.add_widget(self.tabs)
+        
+        return self.main_layout
+    
+    def select_files(self, instance):
+        """Simuliert die Auswahl von Audiodateien"""
+        selected = self.file_chooser.selection
+        if selected:
+            self.selected_files_label.text = f'{len(selected)} Dateien ausgewählt'
+            self.log_output.text += f'Dateien ausgewählt: {", ".join(selected)}\n'
+        else:
+            self.selected_files_label.text = 'Keine Dateien ausgewählt'
+    
+    def start_training(self, instance):
+        """Simuliert den Trainingsprozess"""
+        if not self.file_chooser.selection:
+            self.log_output.text += 'Fehler: Keine Audiodateien ausgewählt\n'
+            return
+        
+        if not self.model_name_input.text:
+            self.log_output.text += 'Fehler: Kein Modellname eingegeben\n'
+            return
+        
+        self.log_output.text += f'Training gestartet für Modell: {self.model_name_input.text}\n'
+        self.progress_bar.value = 0
+        self.progress_label.text = 'Fortschritt: 0%'
+        
+        # Simuliere Trainingsfortschritt
+        self.train_event = Clock.schedule_interval(self.update_progress, 0.5)
+    
+    def update_progress(self, dt):
+        """Aktualisiert den Fortschrittsbalken"""
+        if self.progress_bar.value >= 100:
+            self.train_event.cancel()
+            self.log_output.text += 'Training abgeschlossen!\n'
+            self.progress_label.text = 'Fortschritt: 100%'
+            return
+        
+        # Zufälliger Fortschritt für die Simulation
+        progress_increment = random.randint(1, 5)
+        new_value = min(self.progress_bar.value + progress_increment, 100)
+        self.progress_bar.value = new_value
+        self.progress_label.text = f'Fortschritt: {int(new_value)}%'
+        
+        # Log-Updates
+        if new_value % 10 == 0:
+            self.log_output.text += f'Training bei {int(new_value)}% abgeschlossen...\n'
+    
+    def select_model(self, instance):
+        """Simuliert die Auswahl eines Modells"""
+        # In einer echten App würde hier ein Dateiauswahldialog erscheinen
+        models = ['Modell_1', 'Modell_2', 'Modell_3']
+        selected_model = random.choice(models)
+        self.selected_model_label.text = f'Ausgewähltes Modell: {selected_model}'
+    
+    def start_synthesis(self, instance):
+        """Simuliert die Sprachsynthese"""
+        if self.selected_model_label.text == 'Kein Modell ausgewählt':
+            self.tts_status.text = 'Fehler: Kein Modell ausgewählt'
+            return
+        
+        if not self.text_input.text:
+            self.tts_status.text = 'Fehler: Kein Text eingegeben'
+            return
+        
+        self.tts_status.text = 'Sprachsynthese läuft...'
+        
+        # Simuliere Verarbeitungszeit
+        Clock.schedule_once(self.finish_synthesis, 2)
+    
+    def finish_synthesis(self, dt):
+        """Simuliert den Abschluss der Sprachsynthese"""
+        self.tts_status.text = 'Sprachsynthese abgeschlossen'
+        self.play_button.disabled = False
+    
+    def play_audio(self, instance):
+        """Simuliert das Abspielen der generierten Audiodatei"""
+        self.tts_status.text = 'Audio wird abgespielt...'
+        Clock.schedule_once(lambda dt: setattr(self.tts_status, 'text', 'Audio abgespielt'), 2)
+
+if __name__ == '__main__':
+    VoiceCloningApp().run()
